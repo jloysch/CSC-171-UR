577c2367e7396cf292e9a2f9a7e36daeb3544390
jloysch <47933438+jloysch@users.noreply.github.com>
2019-10-13 23:06:51 -0400

    v2

--- CSC 171 UR - Homework 5/src/com/jloysch/HomeworkFiveDriver.java
+++ CSC 171 UR - Homework 5/src/com/jloysch/HomeworkFiveDriver.java
@@ -18,28 +18,18 @@ public class HomeworkFiveDriver {
 		Element y = new Element("LITHIUM", "Li", 3);
 		Element z = new Element("OXYGEN", "O", 16, 15.999);
 		
-<<<<<<< Upstream, based on origin/master
 		Animal dog = new Animal("Dog", "Jack-Terrier");
 		Animal cat = new Animal("Cat");
 		Animal sugar_glider = new Animal("Sugar Glider");
-=======
 		w.setAtomicWeight(169.934);
 		//x.setAtomicWeight(1.00784);
 		y.setAtomicWeight(6.491);
 		//z.setAtomicWeight(15.999);
 		
-		Animal dog = new Animal("DOG", "Jack-Terrier");
-		Animal cat = new Animal("CAT");
->>>>>>> 3418824 Initial Commit
-		
-<<<<<<< Upstream, based on origin/master
-		dog.setNickname("Spot");
-
-=======
 		cat.setBreed("Tabby");
 		
 		//Person Object
->>>>>>> 3418824 Initial Commit
+
 		System.out.println(a + "\n");
 		System.out.println(b + "\n");
 		
@@ -52,7 +42,6 @@ public class HomeworkFiveDriver {
 		//Animals
 		System.out.println(dog + "\n");
 		
-<<<<<<< Upstream, based on origin/master
 		dog.speak(true);
 		
 		System.out.println("\n" + cat + "\n");
@@ -61,8 +50,6 @@ public class HomeworkFiveDriver {
 		
 		sugar_glider.speak(true);
 		
-=======
->>>>>>> 3418824 Initial Commit
 	}
 
 }
