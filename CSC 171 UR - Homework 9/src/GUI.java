b959188dcd8178accc6a93ac3d5b64f6d9e2d5fa
jloysch <47933438+jloysch@users.noreply.github.com>
2019-11-07 12:23:37 -0500

    Completed

---
+++ CSC 171 UR - Homework 9/src/GUI.java
@@ -0,0 +1,59 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+
+public class GUI extends JFrame implements ActionListener {
+	
+	private JButton SWITCH, PRESS_ME;
+	private JLabel BUTTON_PRESSES_INDICATOR;
+	private int PRESSES;
+	
+	public GUI() {
+		setLayout(new FlowLayout());
+		setSize(new Dimension(800,800));
+		setMinimumSize(new Dimension(300,300));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setupGUIElements();
+		setVisible(true);
+	}
+	
+	void setupGUIElements() {
+		this.SWITCH = new JButton();
+		this.SWITCH.setText("Off");
+		
+		this.PRESS_ME = new JButton();
+		this.PRESS_ME.setText("Press me!");
+		
+		this.BUTTON_PRESSES_INDICATOR = new JLabel();
+		this.BUTTON_PRESSES_INDICATOR.setText(String.valueOf("You've hit the button " + this.PRESSES + " times."));
+		
+		this.SWITCH.addActionListener(this);	
+		this.PRESS_ME.addActionListener(this);
+		
+		add(this.PRESS_ME);
+		add(this.BUTTON_PRESSES_INDICATOR);
+		add(this.SWITCH);
+	}
+	
+	String switchButton() {
+		return SWITCH.getText().equals("On") ? "Off" : "On";
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e);
+		if (e.getSource() == this.SWITCH) {
+			SWITCH.setText(switchButton());
+		} else {
+			this.BUTTON_PRESSES_INDICATOR.setText(String.valueOf("You've now hit the button " + ++this.PRESSES + " time(s)!"));
+		}
+	}
+	
+	public static void main(String args[]) {
+		GUI gui = new GUI();
+	}
+}
